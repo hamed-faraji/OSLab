5f420bd328decc4a4b015558b27817742fc5a876
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-16 22:55:26 +0430

    File 4 added

---
+++ 4.java
@@ -0,0 +1,20 @@
+
+
+class Sum_Runner extends Thread{
+	
+	public Sum_Runner(){
+	
+	}
+	public void run()
+    {
+		
+		
+	}
+		
+}
+
+public class oslab962asgn4{
+	public static void main(String[] args){
+		   
+	}
+}
