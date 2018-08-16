35fe92161db57a2e23ab09e49cf19d93ddf812a6
HaMeD <34552848+hamed-faraji@users.noreply.github.com>
2018-08-16 22:59:26 +0430

    Add thread to calculatre sum of 1 to n

--- 4.java
+++ 4.java
@@ -1,12 +1,28 @@
 
 
 class Sum_Runner extends Thread{
+	int Sum, x;
+	boolean type;
+	
 	
 	public Sum_Runner(){
-	
+		this.x = x;
+		this.type = type;
 	}
 	public void run()
     {
+		try
+        {	
+        	if(type) {
+        	for ( int i = 0 ; i < x; i++) {
+				Sum += i;
+			}
+        	System.out.println("Sum is "+ Sum  + " and Thread ID is  " + Thread.currentThread().getId());
+        	}
+        	else
+        		System.out.println("Subtract is:  "+ (x-2)  + "  and Thread ID is:   " + Thread.currentThread().getId());
+        	}
+        catch (Exception e){}
 		
 		
 	}
